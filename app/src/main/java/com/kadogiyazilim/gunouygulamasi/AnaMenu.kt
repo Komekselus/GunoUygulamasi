42e040008c4ceef48c0f79b8dfd1ea696ad1a7b8
Komekselus <kadirek.062021@gmail.com>
2023-05-01 19:56:29 +0300

    Initial commit

---
+++ app/src/main/java/com/kadogiyazilim/gunouygulamasi/AnaMenu.kt
@@ -0,0 +1,86 @@
+package com.kadogiyazilim.gunouygulamasi
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+import java.util.Calendar
+
+class AnaMenu : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    var databaseReference: DatabaseReference?=null//başlangıç değeri boş olabilir
+    var database: FirebaseDatabase?=null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ana_menu)
+        // TextView öğesini tanımlayın
+        val textView = findViewById<TextView>(R.id.tarih)
+
+// Calendar sınıfını kullanarak bugünkü tarihi alın
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH) + 1 // 0-11 arası olduğu için +1 eklenir
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+// Tarihi TextView'a yazdırın
+        val todayDate = "$day/$month/$year"
+        textView.text = todayDate
+
+
+
+        auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance()
+        databaseReference = database?.reference!!.child("Kullanici")
+        val currentUser = auth.currentUser
+        val adSoyad = findViewById<TextView>(R.id.anaMenuAdSoyad)
+
+        //realtime databasedeki idye ulaşıp altındaki childların içindeki veriyi isim soyisim yerine aktarıcaz
+        val userReference = databaseReference?.child(currentUser?.uid!!)
+        userReference?.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                adSoyad.text = snapshot.child("AdiSoyadi").value.toString()
+            }
+            override fun onCancelled(error: DatabaseError) {
+            }
+        })
+        // takvim sayfasına gidiyoruz
+        val takvimButon = findViewById<ImageButton>(R.id.btnAnaMenuTakvim)
+        takvimButon.setOnClickListener {
+            val intent = Intent(applicationContext,Takvimim::class.java)
+            startActivity(intent)
+        }
+
+        /*hesabımı sil
+        * currentUser?.delete()?.addOnCompleteListener{
+        *       if(it.isSuccseful){
+        *           "Silindi Mesajı"
+        *             auth.singOut()
+        * v                 ar currentUserDb = databaseReference?.child(currentUser.uid)
+                            currentUserDb?.removeValue()
+        *              "giriş sayfasına gönder"}
+        *           }
+        *
+        *
+        *
+        *
+        *
+        * */
+
+
+        // Çıkış yap butonu
+        // cikisyapbutonu.setOnClickListener {
+        //auth.signOut()
+        // startActivity(Intent(this@anaMenu,GirisActivity::class.java))
+        //finish()
+        // }
+
+
+
+    }
+}
---
+++ app/src/main/java/com/kadogiyazilim/gunouygulamasi/Takvimim.kt
@@ -0,0 +1,17 @@
+package com.kadogiyazilim.gunouygulamasi
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.CalendarView
+
+class Takvimim : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_takvimim)
+        val calendar =findViewById<CalendarView>(R.id.calendarView)
+
+
+
+    }
+}
